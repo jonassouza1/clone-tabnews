988a31b1ed3ef92b0601d9309737a4e9628d2eca
jonassouza1 <127683434+jonassouza1@users.noreply.github.com>
2024-02-28 17:05:35 +0000

    I modified the connection to the database

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   try {
     await client.connect();
@@ -22,12 +22,3 @@ async function query(queryObject) {
 export default {
   query: query,
 };
-
-function getSSLValues() {
-  if (process.env.POSTGRES_CA) {
-    return {
-      ca: process.env.POSTGRES_CA,
-    };
-  }
-  return process.env.NODE_ENV === "development" ? false : true;
-}
