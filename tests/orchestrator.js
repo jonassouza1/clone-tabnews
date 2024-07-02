098ba64909117931306962148ff3afd87f648adb
jonassouza1 <127683434+jonassouza1@users.noreply.github.com>
2024-07-02 20:07:54 +0000

    --amend

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,16 +1,20 @@
 import retry from "async-retry";
 
-async function waitForAllServices() {}
-await waitForWebServer();
+async function waitForAllServices() {
+  await waitForWebServer();
+}
 
 async function waitForWebServer() {
   return retry(fatchStatusPage, {
     retries: 100,
+    maxTimeout: 1000,
   });
 
   async function fatchStatusPage() {
     const response = await fetch("http://localhost:3000/api/v1/status");
-    const responseBody = await response.json();
+    if (response.status !== 200) {
+      throw Error();
+    }
   }
 }
 export default {
