b37d02701ebbb1f962639d91107f546dde3d916a
jonassouza1 <127683434+jonassouza1@users.noreply.github.com>
2025-08-03 22:34:24 +0000

    feat: feat: implement `/api/v1/user` endpoint with session renewal

--- infra/controller.js
+++ infra/controller.js
@@ -5,6 +5,19 @@ import {
   NotFoundError,
   UnauthorizedError,
 } from "infra/errors";
+import * as cookie from "cookie";
+import session from "models/session.js";
+
+async function setSessionCookie(sessionToken, response) {
+  const setCookie = cookie.serialize("session_id", sessionToken, {
+    path: "/",
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  response.setHeader("Set-Cookie", setCookie);
+}
 
 async function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -30,6 +43,7 @@ const controller = {
     onNoMatch: onNoMatchHandler,
     onError: onErrorHandler,
   },
+  setSessionCookie,
 };
 
 export default controller;
