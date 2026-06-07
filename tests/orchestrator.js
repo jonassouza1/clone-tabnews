9d0ab6292c6961d46095fb94e52949da156d7008
jonassouza1 <127683434+jonassouza1@users.noreply.github.com>
2026-06-07 02:26:47 +0000

    feat: add `activation.findOneValidById()` and `orchestrator.extractUUID()`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -78,6 +78,10 @@ async function getLastEmail() {
   lastEmailItem.text = emailTextBody;
   return lastEmailItem;
 }
+function extractUUID(text) {
+  const match = text.match(/[0-9a-fA-F-]{36}/);
+  return match ? match[0] : null;
+}
 
 const orchestrator = {
   waitForAllServices,
@@ -87,6 +91,7 @@ const orchestrator = {
   createSession,
   deleteAllEmails,
   getLastEmail,
+  extractUUID,
 };
 
 export default orchestrator;
