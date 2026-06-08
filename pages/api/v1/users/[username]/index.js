ea7d115e101e51b8cc1bfeaafd68c13798240368
jonassouza1 <127683434+jonassouza1@users.noreply.github.com>
2026-06-08 00:29:17 +0000

    feat: consider `resource` in `authorization` model

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -1,6 +1,8 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import user from "models/user.js";
+import authorization from "models/authorization.js";
+import { ForbiddenError } from "infra/errors.js";
 
 const router = createRouter();
 
@@ -20,6 +22,17 @@ async function getHandler(request, response) {
 async function patchHandler(request, response) {
   const userName = request.query.username;
   const userInputValues = request.body;
+
+  const userTryingToPatch = request.context.user;
+  const targetUser = await user.findOneByUsername(userName);
+
+  if (!authorization.can(userTryingToPatch, "update:user", targetUser)) {
+    throw new ForbiddenError({
+      message: "Você não possui permissão para atualizar outro usuário.",
+      action:
+        "Verifique se você possui a feature necessária para atualizar outro usuário.",
+    });
+  }
   const updatedUser = await user.update(userName, userInputValues);
 
   return response.status(200).json(updatedUser);
