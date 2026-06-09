28e206784ef55c9011a4ed00c1afd53af79dbf4f
jonassouza1 <127683434+jonassouza1@users.noreply.github.com>
2026-06-09 16:52:46 +0000

    feat: add sameSite=Lax to `controller.setSessionCookie()`

--- tests/integration/api/v1/sessions/post.test.js
+++ tests/integration/api/v1/sessions/post.test.js
@@ -132,6 +132,7 @@ describe("POST /api/v1/sessions", () => {
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         path: "/",
         httpOnly: true,
+        sameSite: "Lax",
       });
     });
   });
